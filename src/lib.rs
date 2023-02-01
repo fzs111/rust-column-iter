d28b335fffbe4665cf954bdb6cd99a610c4b0c08
FZs <zsombor0305fzs@gmail.com>
2023-02-01 21:04:53 +0100

    Fixed getter methods

--- src/lib.rs
+++ src/lib.rs
@@ -57,9 +57,10 @@ impl<'a, T> Iterator for ColumnMutIter<'a, T> {
     }
 }
 
-//INVARIANT: period > 0
-//INVARIANT: offset < period
-//INVARIANT: ptr is always non-null, well-aligned and points to a valid instance of [T]
+//INVARIANT: column_count > 0
+//INVARIANT: offset < column_count
+//INVARIANT: ptr is well-aligned and points to a valid instance of [T]
+//INVARIANT: len * column_count + offset <= [T].len()
 //INVARIANT: all Column structs sharing the same slice of data simultaneously
 //           must have equal `period`s and distinct `offset`s
 pub struct ColumnMut<'a, T>{
@@ -84,30 +85,57 @@ impl<'a, T> ColumnMut<'a, T> {
         index * self.column_count + self.offset
     }
     
-    /*pub fn get_mut(&mut self, index: usize) -> Option<&mut T> {
-        
-    }*/
+    unsafe fn get_ptr(&self, index: usize) -> *const T {
+        //SAFETY: This function must be called with a valid index
+
+        self.ptr.as_ptr().add(self.map_index(index))
+    }
+    pub fn get_mut(&mut self, index: usize) -> Option<&mut T> {
+        if index >= self.len() {
+            return None;
+        }
+
+        unsafe{
+            //SAFETY: Access is bound-checked
+
+            //TODO: Ask if `.cast_mut()` is safe here (it should be...)
+            Some(&mut *self.get_ptr(index).cast_mut())
+        }
+    }
+    pub fn get(&self, index: usize) -> Option<&T> {
+        if index >= self.len() {
+            return None;
+        }
+
+        unsafe{
+            //SAFETY: Access is bound-checked
+
+            Some(& *self.get_ptr(index))
+        }
+    }
 }
 
 impl<'a, T> Index<usize> for ColumnMut<'a, T> {
     type Output = T;
     
     fn index(&self, index: usize) -> &Self::Output {
-        unsafe{
-            //FIXME Add bounds checking
-            & *self.ptr.as_ptr().add(self.map_index(index))
-        }
+        let len = self.len();
+
+        self.get(index).unwrap_or_else(|| {
+            panic!("index out of bounds: the len is {len} but the index is {index}")
+        })
     }
 }
 
 impl<'a, T> IndexMut<usize> for ColumnMut<'a, T> {    
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        unsafe{
-            //FIXME Add bounds checking
-            &mut *self.ptr.as_ptr().add(self.map_index(index))
-        }
+        let len = self.len();
+
+        self.get_mut(index).unwrap_or_else(|| {
+            panic!("index out of bounds: the len is {len} but the index is {index}")
+        })
     }
 }
 
-unsafe impl<'a, T> Send for ColumnMut<'a, T> where [T]: Send {}
-unsafe impl<'a, T> Sync for ColumnMut<'a, T> where [T]: Sync {}
+unsafe impl<'a, T> Send for ColumnMut<'a, T> where &'a mut [T]: Send {}
+unsafe impl<'a, T> Sync for ColumnMut<'a, T> where &'a mut [T]: Sync {}
